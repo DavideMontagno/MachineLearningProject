e9027de7540291cfba5627aed0f3699f92455b94
aboffa <a.boffa95@gmail.com>
2020-01-03 09:52:20 +0100

    first plots of the learning curves

--- Model_1_KerasNN/model_1.py
+++ Model_1_KerasNN/model_1.py
@@ -5,11 +5,11 @@ from keras.regularizers import l2
 from keras.optimizers import SGD
 import keras.backend as K
 from sklearn.model_selection import KFold
+import matplotlib.pyplot as plt
 import os
 # Just disables the warning about AVX AVX2
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# https://matplotlib.org/index.html
 
 dataset_tr = numpy.loadtxt('../project/ML-CUP19-TR.csv', delimiter=',', dtype=numpy.float64)
 
@@ -20,7 +20,6 @@ Y = dataset_tr[:, -2:]
 w_initial
 kernel initialization
 '''
-
 
 def euclidean_distance_loss(y_true, y_pred):
     """
@@ -42,7 +41,7 @@ def coeff_determination(y_true, y_pred):
 
 
 # (nesterov only for batch)
-def trainAndEvaluate(x_tr, y_tr, x_ts, y_ts, eta=0.015, alpha=0.7, nEpoch=350, lambda_param=0.01, nLayer=3, nUnitPerLayer=20,
+def trainAndEvaluate(x_tr, y_tr, x_ts, y_ts, eta=0.015, alpha=0.7, nEpoch=350, lambda_param=0.01, nUnitPerLayer=20, nLayer=3, 
           batch_size=100):
     model = Sequential()
     model.add(Dense(20, input_dim=20, kernel_initializer='glorot_normal', activation='sigmoid'))
@@ -54,40 +53,59 @@ def trainAndEvaluate(x_tr, y_tr, x_ts, y_ts, eta=0.015, alpha=0.7, nEpoch=350, l
 
     sgd = SGD(learning_rate=eta, momentum=alpha, nesterov=False)
     model.compile(optimizer=sgd, loss=euclidean_distance_loss, metrics=['mse', 'mae', coeff_determination])
+    #todo add in fit validation set
     history = model.fit(x_tr, y_tr, epochs=nEpoch, batch_size=batch_size, verbose=0)
     score = model.evaluate(x_ts, y_ts, verbose=0)
     return history, score
 
 
-etas = [0.05, 0.1, 0.15]
-alphas = [0.7, 0.75, 0.8]
-nEpocs = [250, 300, 350]
+etas = [0.001, 0.03, 0.7]
+alphas = [0.6, 0.75, 0.8]
+nEpocs = [325, 450]
+lambdas = [0.01,0.02,0.03]
+nUnitLayers = [18,20,23]
+
 for eta in etas:
     for alpha in alphas:
         for nEpoc in nEpocs:
-            kfold = KFold(n_splits=10, random_state=None, shuffle=True)
-            cvscores = []
-            for train_index, test_index in kfold.split(X):
-                x_tr = X[train_index]
-                y_tr = Y[train_index]
-                x_ts = X[test_index]
-                y_ts = Y[test_index]
-                history, score = trainAndEvaluate(x_tr, y_tr, x_ts, y_ts, eta, alpha)
-                # now we consider only the score, not the history of training
-                cvscores.append(score)
-            averageLoss = 0
-            averageMSE = 0
-            averageMAE = 0
-            averageR2 = 0
-            for score in cvscores:
-                averageLoss += score[0]
-                averageMSE += score[1]
-                averageMAE += score[2]
-                averageR2 += score[3]
-            averageLoss /= len(cvscores)
-            averageMSE /= len(cvscores)
-            averageMAE /= len(cvscores)
-            averageR2 /= len(cvscores)
-            print("Eta: " + str(eta) + "  Alpha: " + str(alpha) + " nEpoch: " + str(nEpoc) + " AverageLoss: " + str(
-                averageLoss) + " AverageMSE: " + str(averageMSE) + " AverageMAE: " + str(
-                averageMAE) + " AverageR2: " + str(averageR2))
+            for lambda_param in lambdas:
+                for nUnitLayer in nUnitLayers:
+                    kfold = KFold(n_splits=10, random_state=None, shuffle=True)
+                    cvscores = []
+                    nFold = 0
+                    forLegend = []
+                    for train_index, test_index in kfold.split(X):
+                        x_tr = X[train_index]
+                        y_tr = Y[train_index]
+                        x_ts = X[test_index]
+                        y_ts = Y[test_index]
+                        history, score = trainAndEvaluate(x_tr, y_tr, x_ts, y_ts, eta, alpha,nEpoc,lambda_param,nUnitLayer)
+                        # Plot training loss values (just half of them)
+                        if nFold % 2 == 0:
+                            plt.plot(history.history['loss'])
+                            plt.title('Model loss '+str(eta)+'_' + str(alpha) +'_'+ str(nEpoc)+'_'+str(lambda_param)+'_'+str(nUnitLayer))
+                            plt.ylabel('Loss')
+                            plt.xlabel('Epoch')
+                            #plt.show()
+                            cvscores.append(score)
+                            forLegend.append('Train '+str(nFold))
+                        nFold +=1
+                    plt.legend(forLegend, loc='upper right')
+                    plt.savefig('./plots/learning_curve_'+str(eta)+'_' + str(alpha) +'_'+ str(nEpoc) +'_'+str(lambda_param)+'_'+str(nUnitLayer)+'.png',dpi=1000)
+                    plt.close()
+                    averageLoss = 0
+                    averageMSE = 0
+                    averageMAE = 0
+                    averageR2 = 0
+                    for score in cvscores:
+                        averageLoss += score[0]
+                        averageMSE += score[1]
+                        averageMAE += score[2]
+                        averageR2 += score[3]
+                    averageLoss /= len(cvscores)
+                    averageMSE /= len(cvscores)
+                    averageMAE /= len(cvscores)
+                    averageR2 /= len(cvscores)
+                    print("Eta: " + str(eta) + "  Alpha: " + str(alpha) + " nEpoch: " + str(nEpoc) + " Lambda: " + str(lambda_param) + " nUnitPerLayer: " + str(nUnitLayer)+ " AverageLoss (on validation set): " + str(
+                        averageLoss) + " AverageMSE: " + str(averageMSE) + " AverageMAE: " + str(
+                        averageMAE) + " AverageR2: " + str(averageR2))
