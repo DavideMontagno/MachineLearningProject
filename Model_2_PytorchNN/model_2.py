22cb5969f5d2aa251882ef7991d9150157246c66
Antonio <a.boffa95@gmail.com>
2020-01-23 08:35:52 +0100

    speed up

--- Model_2_PytorchNN/model_2.py
+++ Model_2_PytorchNN/model_2.py
@@ -6,32 +6,22 @@ import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from datetime import datetime
 
-
-
-
 # inizialization
 dataset_tr = numpy.genfromtxt(
     '../project/ML-CUP19-TR.csv', delimiter=',', dtype=numpy.float64)
 X = dataset_tr[:, 1:-2]
 Y = dataset_tr[:, -2:]
 D_in = 20
-nUnitLayer = 25
+nUnitLayers = [30,35]
 pyramid=3
 D_out = 2
 batch_size = 64
-etas = [0.001, 0.002, 0.0015]
-alphas = [0.001, 0.0005]
-lambdas = [0.002]
+etas = [0.003, 0.006, 0.01]
+alphas = [0.8, 0.9]
+lambdas = [0.003,0.01]
 nFold=0
-nEpochs = [110]
+nEpoch = 100
 splits_kfold = 10
-
-#main functions
-def call_loss(y_real, y_pred):
-    sum_tot=0
-    for i in range(len(y_real)):
-        sum_tot += torch.sqrt(torch.sum((y_real[i]-y_pred[i])**2 ))
-    return  torch.div(sum_tot,len(y_real))
 
 class Model(torch.nn.Module):
     def __init__(self, D_in, nUnitLayer, D_out):
@@ -41,9 +31,8 @@ class Model(torch.nn.Module):
         """
         super(Model, self).__init__()
         self.hidden1 = torch.nn.Linear(D_in, nUnitLayer, bias=False)
-       # self.hidden2 = torch.nn.Linear(nUnitLayer, nUnitLayer-1*pyramid, bias=False)
-        self.hidden3 = torch.nn.Linear(nUnitLayer, nUnitLayer-1*pyramid, bias=False)
-        self.output = torch.nn.Linear(nUnitLayer-1*pyramid, D_out, bias=False)
+        self.hidden3 = torch.nn.Linear(nUnitLayer, nUnitLayer - 5, bias=False)
+        self.output = torch.nn.Linear(nUnitLayer - 5, D_out, bias=False)
     def forward(self, x):
         """
         In the forward function we accept a Tensor of input data and we must return
@@ -51,8 +40,6 @@ class Model(torch.nn.Module):
         well as arbitrary operators on Tensors.
         """
         h_relu = F.relu(self.hidden1(x))
-       # h_relu2 = F.relu(self.hidden2(h_relu))
-       #h_relu3= F.relu(self.hidden3(h_relu2))
         h_relu3 = F.relu(self.hidden3(h_relu))
         y_pred = self.output(h_relu3)
         return y_pred
@@ -62,21 +49,13 @@ def init_weights(m):
         m.requires_grad=True
         torch.nn.init.xavier_uniform_(m.weight, gain=1.0)
 
+loss_fn = lambda y_real,y_pred: torch.div(torch.sum(F.pairwise_distance(y_real, y_pred, p=2)),len(y_real))
 
-
-
-loss_fn = lambda y_real,y_pred: call_loss(y_real, y_pred) 
-model = Model(D_in, nUnitLayer, D_out)
-model.apply(init_weights)
-model.cuda()
-print(model)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if(device.type=='cuda'):
     print(torch.cuda.get_device_name(torch.cuda.current_device()),'enabled:',torch.backends.cudnn.enabled)
 
-
-
-for nEpoch in nEpochs:
+for nUnitLayer in nUnitLayers:
     for eta in etas:
         for alpha in alphas:
             for lambda_param in lambdas:
@@ -111,7 +90,6 @@ for nEpoch in nEpochs:
                         loss = torch.zeros(1)
                         for i in range(int(len(x_tr) / batch_size)):
                             # TODO requires_grad=True ???
-                            # TODO: validation set
                             optimizer.zero_grad()
                             l = i * batch_size
                             r = (i + 1) * batch_size
@@ -142,8 +120,7 @@ for nEpoch in nEpochs:
                     for cv_value2 in last_loss_ts:
                         averageLossTs += cv_value2
                     averageLossTs /= len(last_loss_ts)
-                    if nFold % 2 == 0:
-
+                    if nFold % 3 == 0:
                             plt1.plot(score_tr)
                             plt1.plot(score_ts)
                             plt2.plot(range(25,nEpoch),score_tr[25:])
@@ -160,7 +137,7 @@ for nEpoch in nEpochs:
                 fig.legend(forLegend, loc='center right')
                 fig.suptitle('Model loss ' + str(eta) + '_' + str(alpha) + '_' + str(nEpoch) + '_' + str(
                                 lambda_param) + '_' + str(batch_size))
-                fig.savefig('./plots/model_correct_2/learning_curve_' + str(eta) + '_' + str(alpha) + '_' + str(nEpoch) + '_' + str(
+                fig.savefig('./plots/MY_learning_curve_' + str(eta) + '_' + str(alpha) + '_' + str(nEpoch) + '_' + str(
                                                                         lambda_param) + '_' + str(batch_size) + '_' + str(nUnitLayer) + 
                                                                         '_' + str(
                                                                         averageLoss) + '.png', dpi=500)
