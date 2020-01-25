a66d25b7b216b5d0e89b7e913f3afecb453ff481
Antonio <a.boffa95@gmail.com>
2020-01-25 16:38:18 +0100

    Update test.py

--- monk/test.py
+++ monk/test.py
@@ -2,28 +2,47 @@ from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
+from keras import regularizers
 
-dataset_train1 = loadtxt('dataset/monks-1.train', delimiter=' ', usecols=range(1, 8))
-dataset_test1 = loadtxt('dataset/monks-1.test', delimiter=' ', usecols=range(1, 8))
+dataset_train1 = loadtxt('dataset/monks-1.train',
+                         delimiter=' ', usecols=range(1, 8))
+dataset_test1 = loadtxt('dataset/monks-1.test',
+                        delimiter=' ', usecols=range(1, 8))
 
-dataset_train2 = loadtxt('dataset/monks-2.train', delimiter=' ', usecols=range(1, 8))
-dataset_test2 = loadtxt('dataset/monks-2.test', delimiter=' ', usecols=range(1, 8))
+dataset_train2 = loadtxt('dataset/monks-2.train',
+                         delimiter=' ', usecols=range(1, 8))
+dataset_test2 = loadtxt('dataset/monks-2.test',
+                        delimiter=' ', usecols=range(1, 8))
 
-dataset_train3 = loadtxt('dataset/monks-3.train', delimiter=' ', usecols=range(1, 8))
-dataset_test3 = loadtxt('dataset/monks-3.test', delimiter=' ', usecols=range(1, 8))
+dataset_train3 = loadtxt('dataset/monks-3.train',
+                         delimiter=' ', usecols=range(1, 8))
+dataset_test3 = loadtxt('dataset/monks-3.test',
+                        delimiter=' ', usecols=range(1, 8))
 
-x = dataset_test1[:, 1:7]
-x_test = dataset_train1[:, 1:7]
-y = dataset_test1[:, 0]
-y_test = dataset_train1[:, 0]
 
-model = Sequential()
-model.add(Dense(30, input_dim=6, kernel_initializer='random_normal', activation='sigmoid'))
-model.add(Dense(1, activation='linear'))
+def solve_monk(dataset_tr, dataset_ts, nUnit, lr, momntum, nEpoch, batch_size, lambda_):
+    x = dataset_tr[:, 1:7]
+    x_test = dataset_ts[:, 1:7]
+    y = dataset_tr[:, 0]
+    y_test = dataset_ts[:, 0]
 
-sgd = SGD(learning_rate=0.05, momentum=0.8,nesterov=False)
-model.compile(optimizer=sgd, loss='mean_squared_error',metrics=['accuracy'])
-model.fit(x,y,validation_data=(x_test,y_test), epochs=150, batch_size=25,verbose=1)
+    model = Sequential()
+    model.add(
+        Dense(nUnit, input_dim=6, kernel_initializer='glorot_normal', activation='sigmoid'))
+    model.add(Dense(1, activation='linear'))
 
-#loss, accuracy = model.evaluate(dataset_train1[:, 1:7], dataset_train1[:, 0])
-#print('Loss: %.2f' % loss , 'Accuracy: %.2f' %(accuracy*100))
+    sgd = SGD(lr=lr, momentum=momntum, nesterov=False)
+    model.compile(optimizer=sgd, loss='mean_squared_error',
+                  metrics=['accuracy'])
+    history = model.fit(x, y, validation_data=(x_test, y_test),
+                        epochs=nEpoch, batch_size=batch_size, verbose=1)
+    accuracy = history.history['val_acc'][-1]
+    loss = history.history['val_loss'][-1]
+    print('Loss: %.2f' % loss, 'Accuracy: %.2f' % (accuracy*100))
+
+
+solve_monk(dataset_train1, dataset_test1, 12, 0.045, 0.8, 120, 16, 0.0005)
+
+#solve_monk(dataset_train1, dataset_test1, 15, 0.035, 0.8, 220, 10, 0.0005)
+
+#solve_monk(dataset_train1, dataset_test1, 15, 0.035, 0.8, 220, 10, 0.0005)
